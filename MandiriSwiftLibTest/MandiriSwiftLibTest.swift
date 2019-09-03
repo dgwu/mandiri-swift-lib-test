8ccd07f926ddc5340b6c2ed049655096d6539fe4
dgwu <danielgunawanwu@gmail.com>
2019-09-03 08:43:13 +0700

    add readme and license, add test cases

---
+++ MandiriSwiftLibTest/MandiriSwiftLibTest.swift
@@ -0,0 +1,21 @@
+//
+//  MandiriSwiftLibTest.swift
+//  MandiriSwiftLibTest
+//
+//  Created by Daniel Gunawan on 03/09/19.
+//  Copyright © 2019 Daniel Gunawan. All rights reserved.
+//
+
+//import Foundation
+
+public final class MandiriSwiftLibTest {
+    let name = "MandiriSwiftLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## Yuhu
+
+read here
