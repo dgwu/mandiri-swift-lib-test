e1c98cc2ed25fb2bf2529c0beaa115e2a4cd937e
dgwu <danielgunawanwu@gmail.com>
2019-09-03 10:32:07 +0700

    add carthage test

---
+++ Carthage/Build/iOS/MandiriSwiftLibTest.framework/Headers/MandiriSwiftLibTest.h
@@ -0,0 +1,19 @@
+//
+//  MandiriSwiftLibTest.h
+//  MandiriSwiftLibTest
+//
+//  Created by Daniel Gunawan on 03/09/19.
+//  Copyright © 2019 Daniel Gunawan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MandiriSwiftLibTest.
+FOUNDATION_EXPORT double MandiriSwiftLibTestVersionNumber;
+
+//! Project version string for MandiriSwiftLibTest.
+FOUNDATION_EXPORT const unsigned char MandiriSwiftLibTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MandiriSwiftLibTest/PublicHeader.h>
+
+
